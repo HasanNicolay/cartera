5c6d8e5edc8a86b2886ca0e4d09588582295abba
Your Name <you@example.com>
2024-08-21 17:41:41 -0500

    Mapiando entidad rol

--- src/main/java/com/gov/mintic/cartera/ingresoegreso/entity/Rol.java
+++ src/main/java/com/gov/mintic/cartera/ingresoegreso/entity/Rol.java
@@ -1,9 +1,19 @@
 package com.gov.mintic.cartera.ingresoegreso.entity;
 
+import jakarta.persistence.*;
+//import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
 public class Rol {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rol", nullable = false)
     private Long idRol;
+    @Column(name = "descripcion", nullable = false)
     private String descripcion;
+    @Column(name = "estado")
     private boolean estado;
 
     public Long getIdRol() {
